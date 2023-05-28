cb9a58101f7fc571d26cfd457f3dac0f144b40b6
Joaquin <joacodileo@gmail.com>
2023-05-28 14:25:16 -0300

    cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 export default function Card({ nombre, precio, img, id }) {
   return (
-    <div>
-      <p>{nombre}</p>
-      <p>{precio}</p>
-      <img src={img} alt="img" />
+    <div className="cardContainer">
+      <div className="divImg">
+        <img src={img} alt="img" />
+      </div>
+      <p className="titulo">{nombre}</p>
+      <p className="preicio">{precio}</p>
+      <button>Consultar cotización</button>
     </div>
   );
 }
